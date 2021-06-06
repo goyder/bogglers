e3dbef59a8ba674c78e6560d5ab9ec1391038475
Goyder <joshua@goyder.net.au>
2021-06-06 20:35:08 +0800

    :sparkles: Add custom board option

--- cmd/bogglers/main.go
+++ cmd/bogglers/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/goyder/bogglers/pkg/engine"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,6 +13,7 @@ func main() {
 	minLetters := flag.Int("minLetters", 3, "Minimum letters to allow as a word.")
 	maxLetters := flag.Int("maxLetters", 6, "Maximum number of letters to search for.")
 	dictionaryPath := flag.String("dict", "", "Filepath to dictionary.txt file.")
+	boggleBoard := flag.String("board", "", "Implement Boggle board as text, letters comma-separated.")
 
 	// All flags are declared - call it
 	flag.Parse()
@@ -25,9 +27,25 @@ func main() {
 	dictionaryData.Close()
 
 	// Spin up our inputs
+	// First, check if a Boggle board has been provided
+	randomBoard := true
+	boggleLetters := strings.Split(*boggleBoard, ",")
+	var boggleLettersArray [16]string
+	if len(boggleLetters) == 16 {
+		randomBoard = false
+		copy(boggleLettersArray[:], boggleLetters[:16])
+	}
+
+	var letterMapping map[string]string
+	if randomBoard {
+		letterMapping = engine.GenerateRandomNetworkLetterMapping(1)
+	} else {
+		letterMapping = engine.GenerateNetworkLetterMapping(boggleLettersArray)
+	}
+
 	game := engine.BoggleGame{
 		Network:       engine.GenerateNetwork(),
-		LetterMapping: engine.GenerateRandomNetworkLetterMapping(1),
+		LetterMapping: letterMapping,
 		Dictionary:    dictionary,
 	}
 	var trieEngine = engine.TrieSearchEngine{}
